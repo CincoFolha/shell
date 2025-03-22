7b5a22e099dcd9ce4ad5aa38d870e2e1a6489a55
LETRA <pedro.pardim.pc@gmail.com>
2025-03-22 18:49:24 -0300

    Initialize

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void lsh_loop();
+
+int main(int argc, char **argv) {
+  lsh_loop();
+  return EXIT_SUCCESS;
+}
+
+void lsh_loop() {
+  printf("Hella, World!\n");
+}
