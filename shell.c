c9862238d05860546f2539574c8ead91215811d1
LETRA <pedro.pardim.pc@gmail.com>
2025-03-23 01:14:25 -0300

    reading a line

--- shell.c
+++ shell.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LSH_RL_BUFSIZE 1024
+
 void lsh_loop(void);
 char *lsh_read_line(void);
 char **lsh_split_line(char *line);
@@ -29,7 +31,36 @@ void lsh_loop(void) {
 }
 
 char *lsh_read_line(void) {
-  return (char *) malloc(16 * sizeof(char));
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    c = getchar();
+
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 }
 
 char **lsh_split_line(char *line) {
