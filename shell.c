1f2665bc6ebcc9e852f7beccfc6b537f6008215e
LETRA <pedro.pardim.pc@gmail.com>
2025-03-23 13:09:58 -0300

    putting together builtins and processes

--- shell.c
+++ shell.c
@@ -154,6 +154,14 @@ int lsh_exit(char **args) {
 }
 
 int lsh_execute(char **args) {
-  args[0] = NULL;
-  return 1;
+  if (args[0] == NULL) {
+    return 1;
+  }
+  for (int i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
 }
